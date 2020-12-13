0816e4a3fd9c09a5b2c88187382473a465a8ff99
vishalchaubey <vishal_live@hotmail.com>
2020-12-14 01:10:46 +0530

    extended the JWT token time to 10 minute

--- src/main/java/com/example/webforecast/configuraions/TokenUtil.java
+++ src/main/java/com/example/webforecast/configuraions/TokenUtil.java
@@ -60,7 +60,7 @@ public class TokenUtil implements Serializable {
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
 
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 10 * 60 * 60 * 1000))
+				.setExpiration(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
 				.signWith(SignatureAlgorithm.HS512, secret).compact();
 	}
 
